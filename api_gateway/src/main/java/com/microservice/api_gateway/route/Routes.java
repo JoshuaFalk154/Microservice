2a36a58917ae9fb7f7d81b0bbf0123060e38751f
Joshua579 <falkjoshua2200@gmail.com>
2024-09-26 16:45:39 +0200

    api-gateway resilience4j implemented

--- api_gateway/src/main/java/com/microservice/api_gateway/route/Routes.java
+++ api_gateway/src/main/java/com/microservice/api_gateway/route/Routes.java
@@ -3,13 +3,18 @@ package com.microservice.api_gateway.route;
 import com.microservice.api_gateway.filter.UserHeaderFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 import static org.springframework.web.servlet.function.RequestPredicates.path;
@@ -37,6 +42,7 @@ public class Routes {
                 .route(path("/api/v1/product"), http(productServiceUrl))
                 .route(path("/api/v1/product/**"), http(productServiceUrl))
                 .before(userHeaderFilter.addUserHeader())
+                .filter(circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -46,6 +52,7 @@ public class Routes {
                 .route(path("/api/v1/inventory"), http(inventoryServiceUrl))
                 .route(path("/api/v1/inventory/**"), http(inventoryServiceUrl))
                 .before(userHeaderFilter.addUserHeader())
+                .filter(circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -55,6 +62,15 @@ public class Routes {
                 .route(path("/api/v1/order"), http(orderServiceUrl))
                 .route(path("/api/v1/order/**"), http(orderServiceUrl))
                 .before(userHeaderFilter.addUserHeader())
+                .filter(circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request ->
+                        ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service unavailable, please try again later"))
                 .build();
     }
 }
